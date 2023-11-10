b5e0593007fa48a3cb959d653285f10ee07ae075
hyodongE <dlgywjd2822@gmail.com>
2023-11-10 18:15:39 +0900

    코드 합병 완료 및 같은 프로젝트 이름 식별 기능 추가" git push

--- edgeCpsProject/static/js/overviewProcessJs.js
+++ edgeCpsProject/static/js/overviewProcessJs.js
@@ -1,22 +1,59 @@
+function findProjectByName(projectName) {
+  // 모든 로컬 스토리지 아이템 가져오기
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
 
-function savePorjectInfo(){
+    // 프로젝트 이름이 포함된 아이템을 찾음
+    if (key && key.includes("_overviewProcessXML")) {
+      var storedProjectInfo = localStorage.getItem(key);
 
-    var projectName = document.getElementById("project_name").value;
-    var projectDescription = document.getElementById("project_description").value;
-    var projectCategory = document.getElementById("project_category").value;
+      // JSON 문자열을 JavaScript 객체로 변환
+      var projectInfo = JSON.parse(storedProjectInfo);
 
-    // localStorage.setItem(projectName+'_current_processDict', 'overviewProcess'); //현재 작업중인 프로세스 dict저장
+      // 프로젝트 이름 비교
+      if (projectInfo && projectInfo.name === projectName) {
+        return projectInfo;
+      }
+    }
+  }
 
-    var projectInfo = {
-        name: projectName,
-        description: projectDescription,
-        category: projectCategory
-    };
+  // 찾지 못한 경우 null 반환
+  return null;
+}
 
-    localStorage.setItem(projectName+'_overviewProcessXML', JSON.stringify(projectInfo));
+function savePorjectInfo() {
+  var projectName = document.getElementById("project_name").value;
+  var projectDescription = document.getElementById("project_description").value;
+  var projectCategory = document.getElementById("project_category").value;
 
-    // localStorage.setItem(projectName+'_workflowXML',workflowXMLList);
+  // localStorage.setItem(projectName+'_current_processDict', 'overviewProcess'); //현재 작업중인 프로세스 dict저장
 
+  var projectInfo = {
+    name: projectName,
+    description: projectDescription,
+    category: projectCategory,
+  };
+
+  var projectNameToFind = projectName;
+  var foundProjectInfo = findProjectByName(projectNameToFind);
+
+  if (foundProjectInfo) {
+    var confirmSave = confirm(
+      "같은 이름의 프로젝트가 이미 존재합니다. 계속 진행하시겠습니까?"
+    );
+
+    if (!confirmSave) {
+      // 사용자가 취소를 누른 경우 함수 종료
+      return;
+    }
+  } else {
+    console.log("프로젝트를 찾을 수 없습니다.");
+  }
+
+  localStorage.setItem(
+    projectName + "_overviewProcessXML",
+    JSON.stringify(projectInfo)
+  );
 }
 
 // window.onload = function() {
@@ -39,15 +76,13 @@ function savePorjectInfo(){
 //             }
 //             var jsonOverview = JSON.parse(modifiedOverviewData);
 
-                    
 //             document.getElementById("project_name").value = jsonOverview['name'];
 //             // var projectInput = document.getElementById("project_name")
 //             // projectInput.readOnly = true
-            
+
 //             document.getElementById("project_description").value = jsonOverview['description']
 //             document.getElementById("project_category").value = jsonOverview['category'];
-        
-        
+
 //     }
 //     // <div class="form-group">
 //     // 		<textarea class="form-control" id="create_project" name="create_project" >True</textarea>
@@ -81,9 +116,6 @@ function savePorjectInfo(){
 //         div.appendChild(textarea);
 //         container.appendChild(div);
 //         textarea.style.display = 'none';
-
-
-
 
 //     }
 
