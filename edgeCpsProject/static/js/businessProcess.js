712eea98b4397d13c98fee4e9cc6163752c15924
phado <corea4724@gmail.com>
2023-11-15 15:55:41 +0900

    req satisfaction 버그 수정

--- edgeCpsProject/static/js/businessProcess.js
+++ edgeCpsProject/static/js/businessProcess.js
@@ -20,58 +20,107 @@
 // 	return resultArray;
 // }
 
-function extractReq(){
-	var xmlString =window.localStorage.getItem(projectName+'_requirementsProcessXml');
 
+
+// var xpathResult = xmlDoc.evaluate("//object[contains(@label, '<<functional requirement>>')]/@label", xmlDoc, null, XPathResult.ANY_TYPE, null);
+// var extractedValues = [];
+// var node = xpathResult.iterateNext();
+// while (node) {
+//     var match = node.value.match(/<<functional requirement>>\n(.*)/);
+
+//     if (match && match[1]) {
+//         if (match[1].includes('[' || ']')) {
+//             match[1] = match[1].substring(1, match[1].length - 1);
+//         }
+//         extractedValues.push(match[1]);
+//     }
+
+//     node = xpathResult.iterateNext();
+// }
+
+
+
+
+
+
+// function extractReq(){
+// 	var xmlString =window.localStorage.getItem(projectName+'_requirementsProcessXml');
+
+// 	var parser = new DOMParser();
+// 	var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+
+// 	var xpathResult = xmlDoc.evaluate("//object/@label", xmlDoc, null, XPathResult.ANY_TYPE, null);
+// 	// var xpathResult = xmlDoc.evaluate("//object[contains(@label, '<<functional requirement>>')]/@label", xmlDoc, null, XPathResult.ANY_TYPE, null);
+// 	var extractedValues = [];
+// 	var node = xpathResult.iterateNext();
+// 	while (node) {
+// 		var match = node.value.match(/<<functional requirement>>\n(.*)/);
+	
+// 		if (match && match[1]) {
+// 			if(match[1].includes('['||']')){
+// 				match[1] = match[1].substring(1,match[1].length -1);
+// 			}
+// 			extractedValues.push(match[1]);
+// 		}
+		
+// 		node = xpathResult.iterateNext();
+// 	}
+// 	if (node== null){
+// 		var mxCells = xmlDoc.querySelectorAll('mxCell');
+// 		var values = [];
+
+// 		// 각 mxCell 요소에서 value 속성을 추출
+// 		mxCells.forEach(function(mxCell) {
+// 			try{
+// 				var value = mxCell.getAttribute('value');	
+// 			}catch{}
+			
+// 			if(value !=null){
+// 				values.push(value);
+
+// 				var match = value.match(/<<functional requirement>>\n(.*)/);
+			
+// 				if (match && match[1]) {
+// 					if(match[1].includes('['||']')){
+// 						match[1] = match[1].substring(1,match[1].length -1);
+// 					}
+// 					extractedValues.push(match[1]);
+// 				}
+			
+// 			}
+
+// 		});
+// 		return extractedValues
+// 	}
+	
+	
+// 	return extractedValues
+// }
+
+function extractReq() {
+	var xmlString =window.localStorage.getItem(projectName+'_requirementsProcessXml');
 	var parser = new DOMParser();
 	var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
 
-	var xpathResult = xmlDoc.evaluate("//object/@label", xmlDoc, null, XPathResult.ANY_TYPE, null);
+	var objects = xmlDoc.getElementsByTagName('object');
 	var extractedValues = [];
-	var node = xpathResult.iterateNext();
-	while (node) {
-		var match = node.value.match(/<<functional requirement>>\n(.*)/);
-	
+
+	for (var i = 0; i < objects.length; i++) {
+		var object = objects[i];
+		var label = object.getAttribute('label');
+
+		if (label.includes('<<functional requirement>>')) {
+		var match = label.match(/<<functional requirement>>\n(.*)/);
+
 		if (match && match[1]) {
-			if(match[1].includes('['||']')){
-				match[1] = match[1].substring(1,match[1].length -1);
-			}
-			extractedValues.push(match[1]);
+			var value = match[1].replace(/[\[\]]/g, ''); // [와 ]를 제거
+			extractedValues.push(value);
 		}
-		
-		node = xpathResult.iterateNext();
+		}
 	}
-	if (node== null){
-		var mxCells = xmlDoc.querySelectorAll('mxCell');
-		var values = [];
 
-		// 각 mxCell 요소에서 value 속성을 추출
-		mxCells.forEach(function(mxCell) {
-			try{
-				var value = mxCell.getAttribute('value');	
-			}catch{}
-			
-			if(value !=null){
-				values.push(value);
-
-				var match = value.match(/<<functional requirement>>\n(.*)/);
-			
-				if (match && match[1]) {
-					if(match[1].includes('['||']')){
-						match[1] = match[1].substring(1,match[1].length -1);
-					}
-					extractedValues.push(match[1]);
-				}
-			
-			}
-
-		});
-		return extractedValues
-	}
-	
-	
-	return extractedValues
-}
+  return extractedValues;
+  }
 
 function previous(){
 	var xmlData = localStorage.getItem(projectName+'_requirementsProcessXml')
