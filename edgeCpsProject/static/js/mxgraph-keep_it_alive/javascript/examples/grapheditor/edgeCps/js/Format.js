4dd5308c09e2a6a998ce96965e6bc51d2411a1de
soonwoo <Edith@soonEdith.local>
2023-11-18 02:02:09 +0900

    req process 우측 속성값 오류 해결

--- edgeCpsProject/static/js/mxgraph-keep_it_alive/javascript/examples/grapheditor/edgeCps/js/Format.js
+++ edgeCpsProject/static/js/mxgraph-keep_it_alive/javascript/examples/grapheditor/edgeCps/js/Format.js
@@ -584,7 +584,9 @@ Format.prototype.refresh = function(arguments)
 		try{
 			// var reqAttribute = selectedCell[0].cells[0].value.attributes[1]['name']+' : '+selectedCell[0].cells[0].value.getAttribute('text');
 			// showAttribute.textContent = reqAttribute;
-			var attributeKey = selectedCell[0].cells[0].value.attributes[3]['nodeName']
+
+			// var attributeKey = selectedCell[0].cells[0].value.attributes[3]['nodeName']
+			var attributeKey = 'text';
 			var attributeKeyContainer = document.createElement('div');
 			attributeKeyContainer.style.marginLeft = '1px';
 			attributeKeyContainer.style.marginRight = '1px';
